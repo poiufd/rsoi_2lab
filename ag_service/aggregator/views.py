8dc5c99d5cb9e7588807c0d8fa860fa10e0a2e75
poiufd <s.volk95@mail.ru>
2018-01-11 11:57:08 +0300

    everything except delete method

--- ag_service/aggregator/views.py
+++ ag_service/aggregator/views.py
@@ -9,6 +9,12 @@ import logging
 from ag_service.celery import app,work_with_products
 from django.shortcuts import render
 import json
+from django.template import loader
+from rest_framework.renderers import TemplateHTMLRenderer
+from django.http import Http404
+from django.http import HttpResponse
+from django.template import RequestContext
+
 
 url_buys = 'http://localhost:8000/buys/'
 url_products = 'http://localhost:8001/products/'
@@ -24,7 +30,7 @@ class AggUserBuysView(APIView):
             r = requests.get(url_buys+"user/"+str(user_id)+"/" + str(order_id)+"/")
             r.raise_for_status()
             dict = r.json(object_pairs_hook=OrderedDict)
-            ids = set(dict.get('products_id'))
+            ids = dict.get('products_id')
             list = []
 
             # here add degradation
@@ -35,25 +41,40 @@ class AggUserBuysView(APIView):
                 dict.update({'products_id': list})
             except requests.exceptions.RequestException:
                 dict.update({"detail": "Service temporarily unavailable."})
-                return Response(dict, status=status.HTTP_200_OK)
+                #return Response(dict, status=status.HTTP_200_OK)
+                print(dict)
+                render(request, 'order_detail.html', {'result':dict})
 
         except requests.exceptions.HTTPError as e:
             status_code = e.response.status_code
-            return Response(r.json(), status=status_code)
+            #return Response(r.json(), status=status_code)
+            return HttpResponse(loader.render_to_string( str(status_code)+'.html'), status=status_code)
         except requests.exceptions.RequestException:
-            return Response({"detail": "Service temporarily unavailable."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+            #return Response({"detail": "Service temporarily unavailable."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+            return HttpResponse(loader.render_to_string('503.html'), status=503)
 
         logging.info(u"Show order details")
-        return Response(dict, status=status.HTTP_200_OK)
+        #return Response(dict, status=status.HTTP_200_OK)
+        return render(request, 'order_detail.html', {'result':dict})
 
-    def patch(self, request, user_id, order_id, format=None):
+
+    def post(self, request, user_id, order_id, format=None):
         try:
             r = requests.get(url_buys+"user/"+str(user_id)+"/" + str(order_id)+"/")
             r.raise_for_status()
-            data = JSONParser().parse(request)
+
+            pre_data = request.POST.get("products_id")
+
+            #data = JSONParser().parse(request)
+
+            #data validation
+            if not pre_data.isdigit():
+                return render(request, 'order_detail.html', {'result':r.json().update({"detail": "Validation error."})} )
+
             dict = r.json(object_pairs_hook=OrderedDict)
             prev_id = dict.get('products_id')
-            ids = data.get('products_id')
+            #ids = data.get('products_id')
+            ids = [int(pre_data)]
 
             for id in ids:
                 r = requests.get(url_products + str(id) + "/")
@@ -81,7 +102,8 @@ class AggUserBuysView(APIView):
                     temp.update({'count': (c+1)})
                     r = requests.patch(url_products + str(id) + "/",temp)
                 logging.info(u"Patch products reversed")    
-                return Response({"detail": "Service temporarily unavailable."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)    
+                #return Response({"detail": "Service temporarily unavailable."}, status=status.HTTP_503_SERVICE_UNAVAILABLE) 
+                return HttpResponse(loader.render_to_string('503.html'), status=503)   
 
 
             dict.update({'products_id': prev_id})
@@ -90,16 +112,20 @@ class AggUserBuysView(APIView):
 
         except requests.exceptions.HTTPError as e:
             status_code = e.response.status_code
-            return Response(r.json(), status=status_code)
+            #return Response(r.json(), status=status_code)
+            return HttpResponse(loader.render_to_string( str(status_code)+'.html'), status=status_code)
         except requests.exceptions.RequestException:
-            return Response({"detail": "Service temporarily unavailable."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+            #return Response({"detail": "Service temporarily unavailable."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+            return HttpResponse(loader.render_to_string('503.html'), status=503) 
 
         logging.info(u"Edit order details")
-        return Response(r.json())
+        #return Response(r.json())
+        return render(request, 'success.html', {'result':r.json()})
+
 
 class AggDeleteOrder(APIView):
 
-    def patch(self, request, user_id, order_id, product_id, format=None):
+    def post(self, request, user_id, order_id, product_id, format=None):
         try:
             r = requests.get(url_buys +"user/"+ str(user_id)+"/" + str(order_id)+"/")
             r.raise_for_status()
@@ -129,19 +155,21 @@ class AggDeleteOrder(APIView):
 
 class AggUserAllBuysView(APIView):
 
-    #template = 'index.html'
+    #renderer_classes = [TemplateHTMLRenderer]
+    #template_name = 'index.html'
 
     def get(self, request, user_id, format=None):
         try:
             r = requests.get(url_buys+"user/"+str(user_id)+"/")
             r.raise_for_status()
-
         except requests.exceptions.HTTPError as e:
             status_code = e.response.status_code
-            return Response(r.json(), status=status_code)
+            #return Response(r.json(), status=status_code)
+            return HttpResponse(loader.render_to_string( str(status_code)+'.html'), status=status_code)
         except requests.exceptions.RequestException:
-            return Response({"detail": "Service temporarily unavailable."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+            #return Response({"detail": "Service temporarily unavailable."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+            return HttpResponse(loader.render_to_string('503.html'), status=503)
         logging.info(u"Show orders")
-
-        return Response(r.json(object_pairs_hook=OrderedDict), status=status.HTTP_200_OK)
-        #return render(request, self.template, {'result':r.json(object_pairs_hook=OrderedDict)})
+        #return Response(r.json(object_pairs_hook=OrderedDict), status=status.HTTP_200_OK)
+        #return Response({'result':r.json(object_pairs_hook=OrderedDict)})
+        return render(request, 'orders.html', {'result':r.json(object_pairs_hook=OrderedDict)})
